e639a411153f4d96e5a2ef21eb2eb9105b3dc04e
DoudGaya <arbdoule@yahoo.com>
2022-12-23 12:17:42 +0100

    something good

--- src/pages/home/Partners.jsx
+++ src/pages/home/Partners.jsx
@@ -9,20 +9,20 @@ import cma from '../../assets/imgs/partners/cma-cgm.png'
 const Partners = () => {
     return (
         <>
-        <div className=" w-full bg-gray-200 gap-6 grid grid-cols-4 px-[200px] ">
-            <a href="#" className=' h-24 flex items-center justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
+        <div className=" w-full overflow-x-auto md:bg-gray-200 snap-x md:gap-6 grid my-20 md:my-0 grid-cols-4 md:px-[200px] ">
+            <a href="#" className=' h-20  py-4 snap-center overflow-hidden  md:h-24 items-center flex justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
                 <img src={mearsk} alt="" srcset="" />
             </a>
 
-            <a href="#" className=' h-24 flex items-center justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
+            <a href="#" className=' h-20  py-4 overflow-hidden  md:h-24 flex items-center justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
                 <img src={hapag} alt="" srcset="" />
             </a>
 
-            <a href="#" className=' h-24 flex items-center justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
+            <a href="#" className=' h-20  py-4 overflow-hidden  md:h-24 flex items-center justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
                 <img src={evergreen} alt="" srcset="" />
             </a>
 
-            <a href="#" className=' h-24 flex items-center overflow-hidden justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
+            <a href="#" className=' h-20  py-4 overflow-hidden  md:h-24 flex items-center justify-center hover:bg-blue-500/30 px-4 border-b-2 hover:border-blue-600'>
                 <img src={cma} alt="" className=' object-contain' srcset="" />
             </a>
         </div>
