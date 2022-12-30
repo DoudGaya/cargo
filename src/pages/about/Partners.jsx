b5b4e071c7fca984e7dd3a4e5c0526dc9a75ceba
DoudGaya <arbdoule@yahoo.com>
2022-12-30 17:15:13 +0100

    partners

--- src/pages/about/Partners.jsx
+++ src/pages/about/Partners.jsx
@@ -1,42 +1,14 @@
-import evergreen from '../../assets/imgs/partners/evergreen.png'
-import hapag from '../../assets/imgs/partners/hapag.png'
-import cma from '../../assets/imgs/partners/cma-cgm.png'
-import maersk from '../../assets/imgs/partners/mearsk.png'
 
-
-
-const partners = [
-    {
-        id: 1,
-        company: 'Mearsk',
-        logo: evergreen,
-    },
-    {
-        id: 1,
-        company: 'cma-cgm',
-        logo: cma,
-    },
-    {
-        id: 1,
-        company: 'Hapag-Lloyd',
-        logo: hapag,
-    },
-    {
-        id: 1,
-        company: 'Mearsk',
-        logo: maersk,
-    },
-]
-
+import partners from '../../partners'
 
 
 
 const Logos = ( { part } ) => {
     return (
         <>
-        <div className=" bg-yellow-200 flex items-center justify-center py-2 px-6">
-            <img src={part.logo} className=" h-[100px] object-contain " alt="" />
-        </div>
+        <a href={part.url} target="_blank" className="flex items-center hover:bg-gray-200 rounded-lg justify-center py-2 px-6">
+            <img src={part.logo} className=" grayscale cursor-pointer hover:grayscale-0 h-[100px] object-contain " alt="" />
+        </a>
         </>
     )
 }
@@ -52,7 +24,7 @@ export default function Partners() {
     return (
         <>
 
-        <div className=" flex w-full bg-yellow-300  py-10">
+        <div className=" flex w-full   py-20">
             <div className=" w-8/12 mx-auto grid gap-2 grid-cols-4">
                 {logo}
             </div>
---
+++ src/partners.js
@@ -0,0 +1,38 @@
+import evergreen from './assets/imgs/partners/evergreen.png'
+import hapag from './assets/imgs/partners/hapag.png'
+import cma from './assets/imgs/partners/cma-cgm.png'
+import maersk from './assets/imgs/partners/mearsk.png'
+
+
+const partners = [
+    {
+        id: 1,
+        url: 'https://www.evergreen-marine.com/' ,
+        company: 'Evergreen',
+        logo: evergreen,
+    },
+
+    {
+        id: 1,
+        url: 'https://www.cma-cgm.com/' ,
+        company: 'cma-cgm',
+        logo: cma,
+    },
+
+    {
+        id: 1,
+        url: 'https://www.hapag-lloyd.com/en/home.html' ,
+        company: 'Hapag-Lloyd',
+        logo: hapag,
+    },
+
+    {
+        id: 1,
+        url: 'https://www.maersk.com/' ,
+        company: 'Mearsk',
+        logo: maersk,
+    },
+]
+
+
+export default partners
