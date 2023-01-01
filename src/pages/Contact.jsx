4f692658391f53f6d457433b296fc3fcaabfff9d
DoudGaya <arbdoule@yahoo.com>
2023-01-01 15:05:21 +0100

    some

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,8 +2,8 @@ import Banner from "../assets/imgs/banner3.jpg";
 const Contact = () => {
     return (
         <>
-        <div className=" w-full flex lex-col ">
-            <div className=" w-full h-[500] bg-gradient-to-r from-blue-600 to-sky-600  ">
+        <div className=" w-full flex lex-col">
+            <div className="w-full h-[500] bg-gradient-to-r from-blue-600 to-sky-600" style={{backgroundImage: `url(${Banner})`}}>
                 When things goes wrong
             </div>
             Contact Us Here...
