2ba5abc1fa872ac6cd0d1544e09bdf7275d5e0c4
DoudGaya <arbdoule@yahoo.com>
2023-01-03 11:52:24 +0100

    contacts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,7 @@ const locations = [
         id: 1,
         country: 'Turkiye Address',
         state: 'Kano',
-        
+
         offices: [
             {
                 state: 'Kano',
@@ -23,8 +23,8 @@ const locations = [
         ],
 
         contacts: [
-            '090 3322 111 22',
-            '0902 223 34 422'
+            '090 TURKEY NUMBER 1',
+            '090 TURKEY NUMBER 2'
         ]
     },
 
@@ -43,8 +43,8 @@ const locations = [
             }
         ],
         contacts: [
-            '090 3322 111 22',
-            '0902 223 34 422'
+            '090 NIGERIAN NUMBER 1',
+            '0902 NIGERIAN NUMBER 2'
         ],
 
     },
@@ -62,13 +62,31 @@ const address = locations.map( (one) => {
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                     </svg>
-                    <p className="text-2xl text-gray-200 py-2">Turkiye Address</p>
+                    <p className="text-2xl text-gray-200 py-2">{ one.country }</p>
                     </div>
                         <hr />
-                        <p className=" text-white leading-7 my-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur fugit vitae doloribus sequi eum ipsam consequatur laudantium fuga, veniam repudiandae.</p>
+
+                        { one.offices.map( office => { 
+                            return (
+                                <>
+                                 <div className=" text-white leading-7 my-4">
+                                    <h3 className=" font-bold my-2">{office.state}</h3>
+                                    <p className=" ml-3"> {office.address}</p>
+                                </div>
+                                </>
+                            ) 
+                            
+                        } )}
+
+
+                       
                     <div className=" flex space-x-4">
-                        <p className=" text-yellow-300 text-sm px-6 py-2 bg-blue-700 font-bold">080 6224 98 34</p>
-                        <p className=" text-yellow-300 text-sm px-6 py-2 bg-blue-700 font-bold">080 6224 98 34</p>
+
+                        {one.contacts.map( (contact) => {
+                            return (
+                                <p className=" text-yellow-300 text-sm px-6 py-2 bg-blue-700 font-bold">{contact}</p>
+                            )
+                        } )}
                     </div>
             </div>
         </>
@@ -82,15 +100,12 @@ const Contact = () => {
         <div className="">
         {/* <ContactBanner /> */}
         <PageBanner banner="Contact" />
-        <div className=" flex h-screen w-8/12 mx-auto flex-col ">
+        <div className=" flex h-screen w-10/12 mx-auto flex-col ">
             <Maps />
             <div className=" grid grid-cols-2 my-10 shadow-lg ">
-                <div className=" flex flex-colspace-y-6 py-10 px-10 bg-blue-600">
-               
-
+                <div className=" flex flex-col space-y-6 py-10 px-10 bg-blue-600">
+                    {address}
                 </div>
-                <div className=""></div>
-
             </div>
         </div>
 
