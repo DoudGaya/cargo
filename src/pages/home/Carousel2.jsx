9cb393f978d8f97733f64affea8bbece9eb18068
DoudGaya <arbdoule@yahoo.com>
2023-04-03 14:01:13 +0100

    build

--- src/pages/home/Carousel2.jsx
+++ src/pages/home/Carousel2.jsx
@@ -1,10 +1,8 @@
 
-import React, { useState }  from 'react';
+import React, { useEffect, useState }  from 'react';
 
 function ImageCarousel ({banner}) {
   // Set state with array containing images
-
-  console.log(banner)
 
 
   const [images, setImages] = useState(banner);
@@ -31,13 +29,18 @@ function ImageCarousel ({banner}) {
      }
   };
 
-
-  console.log(images[index])
+  useEffect(() => {
+    // nextImage()
+    setTimeout(() => {
+      nextImage()
+      console.log('clicked')
+    }, 4000);
+  }, [index] )
 
   return ( 
     <div className='w-full h-full saturate-50 bg-cover bg-center flex bg-no-repeat bg-fixed bg-transparent transition-all duration-200 ease-in-out delay-200' style={{ backgroundImage: `url(${images[index].src})`}} >
-      <button className=' bg-[rgb(24,135,62)] absolute z-50 top-[50%] left-0 px-10 py-2' onClick={nextImage}>Next</button>
-      <button className=' bg-[rgb(24,135,62)] absolute z-50 top-[50%] right-0 px-10 py-2' onClick={previousImage}>Previous</button>
+      {/* <button className=' bg-[rgb(24,135,62)] absolute z-50 top-[50%] left-0 px-10 py-2' onClick={nextImage}>Next</button>
+      <button className=' bg-[rgb(24,135,62)] absolute z-50 top-[50%] right-0 px-10 py-2' onClick={previousImage}>Previous</button> */}
       {/* <img src={images[index].src} width={200} /> */}
     </div>
   );
